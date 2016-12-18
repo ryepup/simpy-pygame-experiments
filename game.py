bbdeca8de87f721310511ca6ee576873b2be2c20
Ryan Davis <ryan@acceleration.net>
2016-12-18 16:01:56 -0500

    Split simulating and rendering
    * start pulling simpy/pygame tools into another file
    * re-render all game objects on a set FPS
    * trade some FP for OOP, which I feel a little mixed about,
       but python OOP is really nice
    
    Might be fun to pull simpygame.py out into an open-source library.
    Will think in that direction as I play around with this stuff more.

--- game.py
+++ game.py
@@ -1,34 +1,6 @@
-from functools import wraps
 import pygame
 import simpy
-from simpy.rt import RealtimeEnvironment
-from math import floor
-
-
-def trace(env, pre_step, post_step):
-    def get_wrapper(step, pre_step, post_step):
-        @wraps(step)
-        def tracing_step():
-            head_event = None
-            if len(env._queue):
-                head_event = env._queue[0]
-
-            pre_step()
-            result = step()
-            post_step(head_event)
-            return result
-        return tracing_step
-
-    env.step = get_wrapper(env.step, pre_step, post_step)
-
-
-def after(fn, post):
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        result = fn(*args, **kwargs)
-        post()
-        return result
-    return wrapper
+from simpygame import (PyGameEnvironment, Renderer)
 
 
 def dim_color(color, percent):
@@ -37,6 +9,7 @@ def dim_color(color, percent):
     result.hsva = (h, s, v * percent, a)
     return result
 
+
 BLACK = pygame.Color(0, 0, 0)
 BLUE = pygame.Color(0, 0, 255)
 RED = pygame.Color(255, 0, 0)
@@ -44,104 +17,69 @@ GREEN = pygame.Color(0, 255, 0)
 YELLOW = pygame.Color(255, 255, 0)
 
 
-class PyGameMonitor(object):
-
-    def __init__(self, env, on_close, screen):
-        self.env = env
-        self.on_close = on_close
-        self.screen = screen
-
-    def pre_step(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.on_close()
-
-    def post_step(self, head_event):
-        txt = 'T: %s' % (self.env.now,)
-        pygame.display.set_caption(txt)
-        pygame.display.flip()
-
-
-class DrawableContainer(simpy.Container):
-
-    def __init__(self, screen, color, center, *args, **kwargs):
-        self.base = super(DrawableContainer, self)
-        self.base.__init__(*args, **kwargs)
-        self.color = color
-        self.center = center
-        self.screen = screen
-        self.get = after(self.get, self.draw)
-        self.put = after(self.put, self.draw)
-
-    def draw(self):
-        pygame.draw.circle(self.screen, BLACK, self.center, 20, 0)
-        pct = self.level / float(self.capacity)
-        pygame.draw.circle(self.screen, self.color,
-                           self.center, int(20 * pct), 0)
-
-
 class Generator(object):
 
-    def __init__(self, env, screen, color, center, tank, battery):
-        self.screen = screen
+    def __init__(self, env, color, center, tank, battery):
         self.center = center
         self.tank = tank
         self.battery = battery
         self.env = env
         self.color = color
 
+        env.renderer.add(self)
+        self.on = False
+
     def run(self):
-        self.draw()
         while True:
             if self.battery.level < self.battery.capacity / 3:
-                self.draw(on=True)
-                yield self.tank.get(1)
-                yield self.env.timeout(120)
-                self.draw(on=False)
-                yield self.battery.put(400)
+                self.on = True
+                yield self.tank.get(10)
+                for i in xrange(10):
+                    yield self.env.timeout(12)
+                    yield self.battery.put(40)
+
+                self.on = False
 
             yield self.env.timeout(20)
 
-    def draw(self, on=False):
-        color = dim_color(self.color, 1 if on else 0.25)
+    def __call__(self, screen):
+        color = dim_color(self.color, 1 if self.on else 0.25)
 
-        pygame.draw.line(self.screen, color, self.center,
+        pygame.draw.line(screen, color, self.center,
                          self.battery.center, 2)
-        pygame.draw.line(self.screen, color, self.center, self.tank.center, 2)
-        pygame.draw.circle(self.screen, color, self.center, 20, 0)
+        pygame.draw.line(screen, color, self.center, self.tank.center, 2)
+        pygame.draw.circle(screen, color, self.center, 20, 0)
 
 
 class Radio(object):
 
-    def __init__(self, env, screen, color, center, battery):
-        self.screen = screen
+    def __init__(self, env, color, center, battery):
         self.center = center
         self.battery = battery
         self.env = env
         self.color = color
 
+        env.renderer.add(self)
+        self.on = False
+
     def run(self):
-        self.draw()
         while True:
             power = battery.get(10)
             result = yield power | self.env.timeout(2)
             if power not in result:
-                self.draw(on=False)
+                self.on = False
                 yield power
 
-            bolt = Lightning(self.env, self.screen,
-                             self.center, self.battery.center)
-            self.env.process(bolt.run())
+#            bolt = Lightning(self.env, self.screen,
+#                             self.center, self.battery.center)
+#            self.env.process(bolt.run())
 
-            self.draw(on=True)
+            self.on = True
             yield self.env.timeout(20)
 
-    def draw(self, on=False):
-        color = dim_color(self.color, 1 if on else 0.25)
-
-#        pygame.draw.line(self.screen, color, self.center,
-#                         self.battery.center, 2)
-        pygame.draw.circle(self.screen, color, self.center, 20, 0)
+    def __call__(self, screen):
+        color = dim_color(self.color, 1 if self.on else 0.25)
+        pygame.draw.circle(screen, color, self.center, 20, 0)
 
 
 class Lightning(object):
@@ -154,7 +92,6 @@ class Lightning(object):
         self.color = BLUE
 
     def run(self):
-        print('firing lightning')
         for i in xrange(0, 10):
             self.draw()
             yield self.env.timeout(0.5)
@@ -163,27 +100,42 @@ class Lightning(object):
         pygame.draw.lines(self.screen, self.color, True,
                           [self.source, self.dest], 3)
 
+
+class CircleContainerRenderer(object):
+
+    def __init__(self, color, center, container):
+        self.color = color
+        self.center = center
+        self.container = container
+
+    def __call__(self, screen):
+        pct = self.container.level / float(self.container.capacity)
+        pygame.draw.circle(screen, self.color, self.center, int(20 * pct), 0)
+
+
 pygame.init()
 
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
+screen.fill(BLACK)
 
-env = RealtimeEnvironment(factor=0.01, strict=False)
-on_closed = env.event()
-fuel = DrawableContainer(screen, RED, (20, 20), env, init=1000, capacity=1000)
-battery = DrawableContainer(screen, BLUE, (20, 80),
-                            env, init=500, capacity=1000)
-generator = Generator(env, screen, GREEN, (80, 80), fuel, battery)
-radio = Radio(env, screen, YELLOW, (20, 160), battery)
+renderer = Renderer(screen)
+
+env = PyGameEnvironment(renderer, factor=0.001, strict=False)
+
+fuel = simpy.Container(env, init=1000, capacity=1000)
+fuel.center = (20, 20)
+renderer.add(CircleContainerRenderer(RED, (20, 20), fuel))
+
+battery = simpy.Container(env, init=500, capacity=1000)
+battery.center = (20, 80)
+renderer.add(CircleContainerRenderer(BLUE, (20, 80), battery))
+
+generator = Generator(env, GREEN, (80, 80), fuel, battery)
+radio = Radio(env, YELLOW, (20, 160), battery)
+
 
 for x in [generator, radio]:
     env.process(x.run())
 
-monitor = PyGameMonitor(env, on_closed.succeed, screen)
-trace(env, monitor.pre_step, monitor.post_step)
-
-screen.fill(BLACK)
-for x in [fuel, battery]:
-    x.draw()
-
-env.run(until=on_closed)
+env.run()
